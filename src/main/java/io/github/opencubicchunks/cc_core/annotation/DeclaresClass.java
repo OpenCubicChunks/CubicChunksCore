8448a66e86019464a09706aae3bdde743eb6b3fc
Tom Martin <tom.martin1239@gmail.com>
2022-08-15 15:59:54 +0100

    Add javadoc to DeclaresClass

--- src/main/java/io/github/opencubicchunks/cc_core/annotation/DeclaresClass.java
+++ src/main/java/io/github/opencubicchunks/cc_core/annotation/DeclaresClass.java
@@ -6,8 +6,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks that a class must be replaced by the class in {@code value}
+ *
+ * If the target class is an interface, use an interface
+ * If the target class is an interface, use an interface
+ *
+ * {@link RetentionPolicy#RUNTIME} to make things easier to debug, it's not actually necessary
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface DeclaresClass {
+    /**
+     * Class to replace it with in the form {@code the.package.name.ClassName}
+     */
     String value();
 }
