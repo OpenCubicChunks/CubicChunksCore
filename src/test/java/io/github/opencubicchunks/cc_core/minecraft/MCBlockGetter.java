a79fcb1acfc01215dc7959ff0b839cee49fd183b
Tom Martin <tom.martin1239@gmail.com>
2022-08-15 15:58:07 +0100

    Remove minecraft package from tests module. Depend on minecraft package from src in tests

--- src/test/java/io/github/opencubicchunks/cc_core/minecraft/MCBlockGetter.java
+++
@@ -1,8 +0,0 @@
-package io.github.opencubicchunks.cc_core.minecraft;
-
-import io.github.opencubicchunks.cc_core.annotation.DeclaresClass;
-
-@DeclaresClass("net.minecraft.world.level.BlockGetter")
-public interface MCBlockGetter extends MCLevelHeightAccessor {
-    MCBlockState getBlockState(MCBlockPos blockPos);
-}
--- src/test/java/io/github/opencubicchunks/cc_core/minecraft/MCBlockState.java
+++
@@ -1,7 +0,0 @@
-package io.github.opencubicchunks.cc_core.minecraft;
-
-import io.github.opencubicchunks.cc_core.annotation.DeclaresClass;
-
-@DeclaresClass("net.minecraft.world.level.block.state.BlockState")
-public abstract class MCBlockState {
-}
