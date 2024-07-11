f6ca000c13282e1fa9c4ace2c9dab567822741e7
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2024-07-12 04:00:41 +1200

    move CloPos into core

--- src/main/java/io/github/opencubicchunks/cc_core/minecraft/MCChunkPos.java
+++ src/main/java/io/github/opencubicchunks/cc_core/minecraft/MCChunkPos.java
@@ -5,11 +5,14 @@ import io.github.opencubicchunks.javaheaders.api.Header;
 @Header
 public class MCChunkPos {
     public final int x, z;
-    public MCChunkPos(int x, int y) {
+    public MCChunkPos(int x, int z) {
         throw new IllegalStateException("Per-version doesn't overwrite method");
     }
 
     public native long toLong();
 
     public native static long asLong(int x, int z);
+
+    public native static int getX(long chunkAsLong);
+    public native static int getZ(long chunkAsLong);
 }
